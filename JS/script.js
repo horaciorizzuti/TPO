f728877aa447e5eac6fa8a8cc8df9d8cd2a97865
horaciorizzuti <111323738+horaciorizzuti@users.noreply.github.com>
2023-05-15 01:13:23 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,70 +1,25 @@
-
 const getDataTime = async ()=>{
-    let address = 'http://api.weatherunlocked.com/api/current/-34.6083,-58.3719?app_id=baca5623&app_key=19dc9d6e97fe3b64cdbac262094b660a';
+    let address = 'https://api.openweathermap.org/data/2.5/weather?lat=-34.6083&lon=-58.3719&appid=824ca28380d4860331856b756ed4d7a4&units=metric';
     let petition = await fetch(address); 
     let resultado = await petition.json(); 
-    let timeC = resultado.temp_c
-    let icon = resultado.wx_icon
+    let timeC = parseInt(resultado.main.temp)
+    let icon = resultado.weather[0].icon
     let txt = "Hoy es un día ideal para un Ristretto"
     let temp = `${txt}  -  Temperatura Amba ${timeC}°C `
-    let dirIcon = `/img/iconnTime/set/${icon}`
-    let dirIconPng = dirIcon.replace(".gif",".png")
+    let dirIcon = `http://openweathermap.org/img/w/${icon}.png`
+    //let dirIcon = `https://openweathermap.org/img/wn/${icon}.png`;
+    
+    let time = 100
+    setTimeout(() => {
+    document.querySelector("#weather").innerHTML = temp;},time);
 
-
-    // console.log(resultado)
-    // console.log(dirIcon)
-    // console.log(temp)
-    // console.log(timeC)
-    // console.log(icon)
-
-    document.querySelector("#weather").innerHTML = temp;
     const img = document.createElement("img");
-    img.src = dirIconPng;
+    img.src = dirIcon;
     img.alt = "clima";
-    img.width = 35;
-    document.querySelector("#weather").appendChild(img);
+    img.width = 45;
+
+    setTimeout(() => {
+    document.querySelector("#weather").appendChild(img);},time);
 }
 
 getDataTime()
-
-
-
-
-// //-------
-
-
-// funciona OK GBA
-// https://api.openweathermap.org/data/2.5/weather?lat=-34.6083&lon=-58.3719&appid=824ca28380d4860331856b756ed4d7a4
-
-// funciona OK GBA - tiene iconos
-// http://api.weatherunlocked.com/api/current/-34.6083,-58.3719?app_id=baca5623&app_key=19dc9d6e97fe3b64cdbac262094b660a
-
-
-// GBA En decimal	-34.6083°, -58.3719°
-// CABA En decimal	-34.599722°, -58.381944°
-
-
-// api.openweathermap.org
-// Key 824ca28380d4860331856b756ed4d7a4
-
-
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-// http://api.weatherunlocked.com/api/forecast/40.7,-74?lang=it&app_id=APP_ID&app_key=APP_KEY
-
-
-
-// http://api.weatherunlocked.com
-
-// Horacio's App
-// App ID
-// baca5623
-// Key
-// 19dc9d6e97fe3b64cdbac262094b660a
-
-// http://api.weatherunlocked.com/api/current/51.50,-0.12?app_id={APP_ID}&app_key={APP_KEY}
-
-
-// funciona OK
-// http://api.weatherunlocked.com/api/current/-34.6083,-58.3719?app_id=baca5623&app_key=19dc9d6e97fe3b64cdbac262094b660a
-
